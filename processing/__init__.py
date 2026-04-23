001ea3d58b390053d7ef8685a47521328d4d45da
rohan-imtiaz <rohanimti@gmail.com>
2026-04-23 17:04:50 +0500

    25280058 on 23/04/2026

---
+++ processing/__init__.py
@@ -0,0 +1,5 @@
+# AI620 VoiceIntent: Processing Package
+
+from processing.transcribe import run_transcription
+from processing.clean import run_cleaning
+from processing.validate import run_validation
